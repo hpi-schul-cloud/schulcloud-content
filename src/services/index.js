f394e0700b145021c313e8aaafa1a9deeda21537
Alexander Kremer <alex@kremer.io>
2017-05-11 17:58:57 +0200

    Added create-hook for json-schema validation

--- src/services/index.js
+++ src/services/index.js
@@ -1,3 +1,5 @@
+const content = require('./content/content.service.js');
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
+  app.configure(content);
 };
---
+++ test/hooks/validate-resource-schema.test.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+const validateResourceSchema = require('../../src/hooks/validate-resource-schema/index');
+
+describe('\'validate-resource-schema\' hook', () => {
+  it('runs the hook', () => {
+    // A mock hook object
+    const mock = {};
+    // Initialize our hook with no options
+    const hook = validateResourceSchema();
+
+    // Run the hook function (which returns a promise)
+    // and compare the resulting hook object
+    return hook(mock).then(result => {
+      assert.equal(result, mock, 'Returns the expected hook object');
+    });
+  });
+});
