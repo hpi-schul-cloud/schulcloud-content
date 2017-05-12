ec34defd60aa7f70d8f0c369eda40a01e34bb1da
Alexander Kremer <alex@kremer.io>
2017-05-12 10:00:03 +0200

    Update content schema and model

--- src/models/content.model.js
+++ src/models/content.model.js
@@ -1,11 +1,17 @@
 // content-model.js - A mongoose model
-// 
+//
 // See http://mongoosejs.com/docs/models.html
 // for more of what you can do here.
 module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
   const content = new mongooseClient.Schema({
-    text: { type: String, required: true },
+    title: { type: String, required: true },
+    // provider: { type: String, required: true },
+    url: { type: String, required: true },
+    licenses: { type: [String], required: true },
+    contentCategory: { type: String, enum: ['atomic', 'learning-object',
+      'proven-learning-object', 'tool'], required: true },
+    mimeType: { type: String, required: true },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
   });
