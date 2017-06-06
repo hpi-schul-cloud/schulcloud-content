0aca46c8587b8b1ccf00642e73f2f513ff0148ac
Alexander Kremer <alex@kremer.io>
2017-06-06 14:43:40 +0200

    add mongodb to elasticseach adapter

---
+++ start-schulcloud-content-mongodb-connector.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+pip install mongo-connector
+pip install elastic2-doc-manager[elastic5]
+
+cat << EOF > /config.json
+{
+   "docManagers": [
+        {
+            "docManager": "elastic2_doc_manager",
+            "targetURL": "elastic:changeme@elasticsearch:9200",
+        }
+    ]
+}
+EOF
+
+# TODO: Find better method for checking, that mongodb and elasticsearch are online
+sleep 10
+
+cd /
+mongo-connector -m mongodb:27017 -t elastic:changeme@elasticsearch:9200 -d elastic2_doc_manager -v
