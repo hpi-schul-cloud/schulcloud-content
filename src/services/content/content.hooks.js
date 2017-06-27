3d356fe27d814ad1121b6b11e3a2b8607c89f028
Alexander Kremer <alex@kremer.io>
2017-06-27 13:59:06 +0200

    Basic Authentication

--- src/services/content/content.hooks.js
+++ src/services/content/content.hooks.js
@@ -1,11 +1,34 @@
 const validateResourceSchema = require('../../hooks/validate-resource-schema/');
 
+function authenticate(hook) {
+
+  const basicAuth = require('basic-auth');
+  const credentials = basicAuth.parse(hook.params.req.headers['authorization']);
+
+  const api = require('../../hooks/authenticate/api');
+  return api.post('/authentication', {
+    json: {
+      username: credentials.name,
+      password: credentials.pass
+    }
+  }).then(data => {
+    const jwtDecode = require('jwt-decode');
+    return jwtDecode(data.accessToken);
+  }).then(_ => {
+    // TODO: Authorization
+    return hook;
+  }).catch(_ => {
+    const errors = require('feathers-errors');
+    throw new errors.NotAuthenticated('Could not authenticate');
+  });
+}
+
 module.exports = {
   before: {
     all: [],
     find: [],
     get: [],
-    create: [validateResourceSchema()],
+    create: [authenticate, validateResourceSchema()],
     update: [],
     patch: [],
     remove: []
