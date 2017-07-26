f61ae462c326edefc0836af28bc7e26be981ad9d
Alexander Kremer <alex@kremer.io>
2017-07-26 15:49:08 +0200

    Update Content Fields

--- src/models/content.model.js
+++ src/models/content.model.js
@@ -5,15 +5,24 @@
 module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
   const content = new mongooseClient.Schema({
-    title: { type: String, required: true },
-    // provider: { type: String, required: true },
+
+    originId: { type: String, unique: true },
+    //userId: {type: mongooseClient.Schema.Types.ObjectId, required: true },
+    providerName: { type: String, required: true },
+
     url: { type: String, required: true },
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    thumbnail: {type: String },
+
+    tags: { type: [String] },
     licenses: { type: [String], required: true },
-    contentCategory: { type: String, enum: ['atomic', 'learning-object',
-      'proven-learning-object', 'tool'], required: true },
+    contentCategory: { type: String, enum: ['atomic', 'learning-object', 'proven-learning-object', 'tool'], required: true },
     mimeType: { type: String, required: true },
+
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
+
   });
 
   return mongooseClient.model('content', content);
