34e3dc1ded6d5b03b995af2a82d2f51e59424bf0
Alexander Kremer <alex@kremer.io>
2017-08-14 08:48:31 +0200

    Reformat Code

--- src/services/content/content.hooks.js
+++ src/services/content/content.hooks.js
@@ -9,26 +9,25 @@ function authenticate(hook) {
   return new Promise((res, rej) => {
     let credentials = basicAuth.parse(hook.params.req.headers['authorization']);
     credentials ? res(credentials) : rej();
-  })
-  // Authenticate against Server
-    .then(credentials => {
-      return api.post('/authentication', {
-        json: {
-          username: credentials.name,
-          password: credentials.pass
-        }
-      });
-      // Parse JWT Token and set UserID
-    }).then(data => {
-      const jwtDecode = require('jwt-decode');
-      const jwtTokenDecoded = jwtDecode(data.accessToken);
-      hook.data.userId = jwtTokenDecoded.userId;
-      return hook;
-      // Auth Error
-    }).catch(_ => {
-      const errors = require('feathers-errors');
-      throw new errors.NotAuthenticated('Could not authenticate');
+  }).then(credentials => {
+    // Authenticate against Backend Server
+    return api.post('/authentication', {
+      json: {
+        username: credentials.name,
+        password: credentials.pass
+      }
     });
+  }).then(data => {
+    // Parse JWT Token and set UserID
+    const jwtDecode = require('jwt-decode');
+    const jwtTokenDecoded = jwtDecode(data.accessToken);
+    hook.data.userId = jwtTokenDecoded.userId;
+    return hook;
+  }).catch(_ => {
+    // Auth Error
+    const errors = require('feathers-errors');
+    throw new errors.NotAuthenticated('Could not authenticate');
+  });
 }
 
 module.exports = {
