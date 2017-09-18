e4d93ad04d7617167a08a56c437bfa391c525d5a
Nicco Kunzmann <niccokunzmann@rambler.ru>
2017-09-18 05:05:05 -0700

    add jsonapi attribute

--- src/jsonapi-content-type.js
+++ src/jsonapi-content-type.js
@@ -10,6 +10,14 @@ module.exports = function jsonapi(req, res) {
     },
     'links': {
       'self': location,
+    },
+    'jsonapi' : {
+      "version" : "1.0",
+      "meta" : {
+        "name": "schulcloud-content",
+        "source" : "https://github.com/schul-cloud/schulcloud-content",
+        "description": "This is the content service for storing learning material."
+      }
     }
   };
   res.append("Location", location);
