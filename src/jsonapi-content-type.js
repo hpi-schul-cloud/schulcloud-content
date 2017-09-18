11c64f29d4ccdd0fe4fe51abe4ceab38a9f8322f
Nicco Kunzmann <niccokunzmann@rambler.ru>
2017-09-18 04:57:21 -0700

    set location header

--- src/jsonapi-content-type.js
+++ src/jsonapi-content-type.js
@@ -1,6 +1,7 @@
 module.exports = function jsonapi(req, res) {
   console.log("resource-v1.service.js: jsonapi", JSON.stringify(res.data))
   var originalResource = JSON.parse(res.data.originalResource); // TODO: if absent, convert attributes back
+  var location = 'http://' + req.headers.host + '/v1/resources/' + res.data._id;
   var result = {
     'data': {
       'id': res.data._id,
@@ -8,8 +9,9 @@ module.exports = function jsonapi(req, res) {
       'attributes': originalResource,
     },
     'links': {
-      'self': 'http://' + req.headers.host + '/v1/resources/' + res.data._id,
+      'self': location,
     }
   };
+  res.append("Location", location);
   res.end(JSON.stringify(result));
 }
