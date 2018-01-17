e43b880ce9fcff52d7f46aa1ede9ae89c9aed274
Alexander Kremer <alex@kremer.io>
2018-01-17 14:34:25 +0100

    Revert "extend feathers-mongoose, so collection count is queried"
    This reverts commit 097a4b534d9eedd6b8dfe9f2b5b2761c32c528be.

--- src/services/resource/resource.service.js
+++ src/services/resource/resource.service.js
@@ -1,42 +1,24 @@
 // Initializes the `content` service on path `/content`
-const { Service } = require('feathers-mongoose');
+const createService = require('feathers-mongoose');
 const createModel = require('../../models/resource.model');
 const hooks = require('./resource.hooks');
 
-class MyService extends Service {
-    find(params) {
-        if(params.query.getCount){
-            delete params.query.getCount
-            return Promise.all([
-                this.Model.db.collections.resources.count(),
-                super.find(params)
-            ]).then(([ count, response ]) => {
-                console.dir(count)
-                response.count = count;
-                return response;
-            })
-        }
-
-        return super.find(params);
-    }
-}
-
 module.exports = function () {
-    const app = this;
-    const Model = createModel(app);
-    const paginate = app.get('paginate');
+  const app = this;
+  const Model = createModel(app);
+  const paginate = app.get('paginate');
 
-    const options = {
-        name: 'resources',
-        Model,
-        paginate
-    };
+  const options = {
+    name: 'resources',
+    Model,
+    paginate
+  };
 
-    // Initialize our service with any options it requires
-    app.use('/resources', new MyService(options));
+  // Initialize our service with any options it requires
+  app.use('/resources', createService(options));
 
-    // Get our initialized service so that we can register hooks and filters
-    const service = app.service('resources');
+  // Get our initialized service so that we can register hooks and filters
+  const service = app.service('resources');
 
-    service.hooks(hooks);
+  service.hooks(hooks);
 };
