fa9ba066a41575c428692100b5e4e656e50245d3
pascal <pascal.fuehrlich@student.hpi.de>
2018-02-28 12:12:06 +0100

    rating model and service

---
+++ src/models/rating-model.js
@@ -0,0 +1,21 @@
+// rating-model.js - A mongoose model
+//
+// See http://mongoosejs.com/docs/models.html
+// for more of what you can do here.
+module.exports = function(app) {
+    const mongooseClient = app.get('mongooseClient');
+    const rating = new mongooseClient.Schema({
+        originId: { type: Number, unique: true, default: Math.random }, //TODO remove
+        materialId: { type: mongooseClient.Schema.ObjectId, ref: 'resource', required: true },
+        rating: { type: Number, min: 0, max: 5, required: true },
+        isTeacherRating: { type: Boolean, required: true },
+        topicId: { type: mongooseClient.Schema.Types.ObjectId, required: true },
+        courseId: { type: mongooseClient.Schema.Types.ObjectId, required: true },
+        createdAt: { type: Date, default: Date.now }
+    });
+
+    return mongooseClient.model('rating', rating);
+};
+
+// get overall average: db.getCollection('ratings').aggregate([{$group: {_id: 0, avgRating: { $avg: "$rating"}}}])
+// get teacher/pupil average: db.getCollection('ratings').aggregate([{$group: {_id: "$isTeacherRating", avgRating: { $avg: "$rating"}}}])
--- src/services/resource/resource.service.js
+++ src/services/resource/resource.service.js
@@ -1,24 +1,24 @@
-// Initializes the `content` service on path `/content`
+// Initializes the `resources` service on path `/resources`
 const createService = require('feathers-mongoose');
-const createModel = require('../../models/resource.model');
+const createResourceModel = require('../../models/resource-model');
+const createRatingModel = require('../../models/rating-model');
 const hooks = require('./resource.hooks');
 
-module.exports = function () {
+module.exports = function() {
   const app = this;
-  const Model = createModel(app);
   const paginate = app.get('paginate');
 
-  const options = {
-    name: 'resources',
-    Model,
-    paginate
-  };
+  const resourceModel = createResourceModel(app);
+  const ratingModel = createRatingModel(app);
 
   // Initialize our service with any options it requires
-  app.use('/resources', createService(options));
+  app.use('/resources', createService({ name: 'resources', Model: resourceModel, paginate }));
+  app.use('/ratings', createService({ name: 'ratings', Model: ratingModel, paginate }));
 
   // Get our initialized service so that we can register hooks and filters
-  const service = app.service('resources');
+  const resourceService = app.service('resources');
+  const ratingService = app.service('ratings');
 
-  service.hooks(hooks);
+  resourceService.hooks(hooks);
+  // ratingService.hooks(hooks); //TODO
 };
