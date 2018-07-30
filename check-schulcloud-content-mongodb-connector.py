c5ba4fcd73623e2bf1666e6b287390993fac8dbb
Alexander Kremer <alex@kremer.io>
2018-07-30 13:44:06 +0200

    Make Connector Service Less Communicative

--- check-schulcloud-content-mongodb-connector.py
+++ check-schulcloud-content-mongodb-connector.py
@@ -3,42 +3,50 @@ import os
 from pymongo import MongoClient
 from elasticsearch import Elasticsearch
 from time import sleep
+import logging
+
+# Only Error Logging
+# See https://github.com/elastic/elasticsearch-py/issues/666
+logging.basicConfig(level=logging.ERROR)
+
+# Total Connection Attempts
+totalConnectionAttempts = 30
 
 # Try connecting to MongoDatabase
 connection = None
 i = 0
-while i < 30:
+while i < totalConnectionAttempts:
 	try:
 			connection = MongoClient([os.environ['MONGO_URI']], int(os.environ['MONGO_PORT']))
 			print("Successfully connected to mongodb")
 			break
 	except:
-			print("Will retry connecting to mongodb")
+			print("Will retry connecting to mongodb {}/{}".format(i+1, totalConnectionAttempts))
 	sleep(0.5)
 	i = i +1
 
-if i == 30:
-    raise ValueError("Connection failed")
+if i == totalConnectionAttempts:
+    raise ValueError("Connection failed for mongodb")
 
 # Try connecting to ElasticSearch
 elasticConnect = Elasticsearch(
 	hosts=[{'host': os.environ['ELASTIC_URI'], 'port': int(os.environ['ELASTIC_PORT'])}]
 )
 i = 0
-while i < 30:
+while i < totalConnectionAttempts:
     try:
-            if not elasticConnect.ping():
-                raise ValueError("Connection failed")
-            else:
-                print("Sucessfully connected to elasticsearch")
-                break
+        if elasticConnect.ping():
+            print("Sucessfully connected to elasticsearch")
+            break
+        else:
+          raise
     except:
-            print("Will retry connecting to elasticsearch")
+        print("Will retry connecting to elasticsearch {}/{}".format(i+1, totalConnectionAttempts))
     sleep(0.5)
     i = i +1
 
-if i == 30:
-    raise ValueError("Connection failed")
+if i == totalConnectionAttempts:
+    raise ValueError("Connection failed for elasticsearch")
 
 # Check whether collection accounts exists in schulcloud db
 exists = connection.admin.command('isMaster')['ismaster']
