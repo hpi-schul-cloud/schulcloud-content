d230be2cc99dd608b52a7b1d8593f8ad30b1ea05
Adrian Jost <adrianjost@users.noreply.github.com>
2019-01-29 18:06:17 +0100

    WIP

---
+++ src/services/files/storage-client.js
@@ -0,0 +1,12 @@
+const pkgcloud = require('pkgcloud');
+
+module.exports = {
+	client: pkgcloud.storage.createClient({
+		provider: 'amazon',
+		keyId: process.env['STORAGE_KEY_ID'] || 'sc-devteam', // access key id
+		key: process.env['STORAGE_KEY'], // secret key
+		//region: 'us-west-2', // region
+		forcePathBucket: true, // CRITICAL option here as we don't follow default https://bucket-name.s3-us-west-2.amazonaws.com url format for endpoint
+		endpoint: process.env['STORAGE_ENDPOINT'] || 'https://dev-storage.schul-cloud.org:9001'
+	})
+};
