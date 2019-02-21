a24742a4e9ae32ba929d902ae8fd0470f141b3c2
Adrian Jost <adrianjost@users.noreply.github.com>
2019-02-21 10:17:04 +0100

    renaming

--- src/services/content_filepaths/content_filepaths.service.js
+++ src/services/content_filepaths/content_filepaths.service.js
@@ -1,7 +1,7 @@
 // Initializes the `content` service on path `/content`
 const createService = require('feathers-mongoose');
-const createModel = require('../../models/file_structure.model');
-const hooks = require('./file_structure.hooks');
+const createModel = require('../../models/content_filepaths.model');
+const hooks = require('./content_filepaths.hooks');
 
 module.exports = function () {
   const app = this;
@@ -9,16 +9,16 @@ module.exports = function () {
   const paginate = app.get('paginate');
 
   const options = {
-    name: 'file_structure',
+    name: 'content_filepaths',
     Model,
     paginate
   };
 
   // Initialize our service with any options it requires
-  app.use('/file_structure', createService(options));
+  app.use('/content_filepaths', createService(options));
 
   // Get our initialized service so that we can register hooks and filters
-  const service = app.service('file_structure');
+  const service = app.service('content_filepaths');
 
   service.hooks(hooks);
 };
