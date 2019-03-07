ea62699fd7ba7644fde5cf821c20329866578e56
Adrian Jost <adrianjost@users.noreply.github.com>
2019-03-07 13:09:20 +0100

    fix distribution service tests

--- test/services/file_distribution.test.js
+++ test/services/file_distribution.test.js
@@ -1,8 +1,27 @@
 const assert = require('assert');
 const app = require('../../src/app');
 const { WritableMock } = require('stream-mock');
+const contentFilepaths = app.service('content_filepaths');
+
+const insertMock = () => {
+  const mockData = {
+    path: 'test.txt',
+    contentId: 'content_mock_id',
+    isTemp: false,
+    createdBy: 'user_mock_id'
+  };
+  // _id must match [a-f0-9]{25} to be accepted by feathers
+  return contentFilepaths.update('5c7510725822a18234e48519', mockData, { mongoose: { upsert: true }});
+};
+
+const removeMock = () => {
+  return contentFilepaths.remove('5c7510725822a18234e48519');
+};
 
 describe('\'files/get*\' service', () => {
+
+  before(insertMock);
+  after(removeMock);
 
   it('registered the service', () => {
     const service = app.service('files/get*');
@@ -16,10 +35,13 @@ describe('\'files/get*\' service', () => {
 
     const resStream = new WritableMock({objectMode: true});
 
-    resStream.on('finish', ()=>{
-      assert.equal(JSON.stringify(resStream.data.join('')), JSON.stringify(expectedResult));
+    return new Promise((resolve, reject) => {
+      resStream.on('finish', ()=>{
+        assert.equal(JSON.stringify(resStream.data.join('')), JSON.stringify(expectedResult));
+        resolve();
+      });
+      resStream.on('error', reject);
+      service.find({req: {params: {'0':'test.txt'}, res: resStream}}).catch(reject);
     });
-
-    return service.find({req: {params: {'0':'test.txt'}, res: resStream}});
   });
 });
