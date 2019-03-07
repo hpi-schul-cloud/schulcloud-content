675482e6bf97c70b00b8580004c990d9985cf95f
Adrian Jost <adrianjost@users.noreply.github.com>
2019-03-07 10:42:54 +0100

    apply db changes to download/distribution service

--- src/services/files/file_distribution.service.js
+++ src/services/files/file_distribution.service.js
@@ -9,23 +9,41 @@ class FileDistributionService {
     this.app = app;
   }
 
-  async find({ req }) {
+  find({ req }) {
     const res = req.res;
     const filePath = req.params['0'].replace(/^\//, '');
-    // check if file exists to prevent crash during download
-    try{
-      const fileInfo = await fileExists(filePath);
-      const contentType = mime.contentType(path.extname(fileInfo.name)); // 'application/json; charset=utf-8'
 
-      if(res.header){
-        if(contentType){   res.header('Content-Type', contentType); }
-        if(fileInfo.etag){ res.header('ETag', fileInfo.etag); }
-      }
+    // get fileId
+    return this.app.service('content_filepaths').find({ query: {
+        path: filePath,
+        isTemp: false
+      } })
+      .then(async fileObjects => {
+        // TODO what happens if we got more than 1 result?
+        if(fileObjects.data.length === 0){
+          throw 'Can\'t find requested file';
+        }
+        const fileId = fileObjects[0]._id;
 
-      return promisePipe(getDownloadStream(filePath), res);
-    }catch(error){
-      return error;
-    }
+        try{
+          // check if file exists to prevent crash during download
+          const fileInfo = await fileExists(fileId);
+
+          const contentType = mime.contentType(path.extname(fileInfo.name)); // 'application/json; charset=utf-8'
+          if(res.header){
+            if(contentType){   res.header('Content-Type', contentType); }
+            if(fileInfo.etag){ res.header('ETag', fileInfo.etag); }
+          }
+
+          return promisePipe(getDownloadStream(fileId), res);
+        }catch(error){
+          return error;
+        }
+
+      })
+      .catch(error => {
+        return error;
+      });
   }
 }
 
