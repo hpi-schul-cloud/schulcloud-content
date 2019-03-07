a16a17b9b6ace0e704086e48a47806d1f43c6bdf
Adrian Jost <adrianjost@users.noreply.github.com>
2019-03-07 14:09:01 +0100

    WIP test fixing
    Co-Authored-By: Elias <xhenet@users.noreply.github.com>

--- test/services/file_manage.test.js
+++ test/services/file_manage.test.js
@@ -2,39 +2,48 @@ const assert = require('assert');
 const app = require('../../src/app');
 const contentFilepaths = app.service('content_filepaths');
 
+const { mockUserId, mockContentId } = require('./mockData');
+
+const deleteMockFiles = [];
+const saveMockFiles = [];
+
 const insertMock = () => {
+  const paths = [
+    `${mockContentId}/index.html`,
+    `${mockContentId}/menue/clip_2_1.html`,
+    `${mockContentId}/menue/clip_3_1.html`,
+    `${mockContentId}/menue/clip_5_1.html`,
+    `${mockContentId}/menue/clip_6_1.html`
+  ];
   const persistentMockData = {
-    fileIds: [
-      'content_mock_id/index.html',
-      'content_mock_id/menue/clip_2_1.html',
-      'content_mock_id/menue/clip_3_1.html',
-      'content_mock_id/menue/clip_5_1.html',
-      'content_mock_id/menue/clip_6_1.html'
-    ],
-    contentId: 'content_mock_id',
-    isTemporary: false,
-    userId: 'user_mock_id'
+    contentId: `${mockContentId}`,
+    isTemp: false,
+    createdBy: mockUserId
   };
-  const insertPersistent = contentFilepaths.create(persistentMockData);
+  const insertPersistent = paths.map(filePath => {
+    contentFilepaths.create({path: filePath, ...persistentMockData}).then(fileObj => {
+      deleteMockFiles.push(fileObj._id);
+    });
+  });
 
   const tempMockData = {
-    fileIds: [
-      'tmp/user_mock_id/content_mock_id/test.txt',
-    ],
-    contentId: 'content_mock_id',
-    isTemporary: true,
-    userId: 'user_mock_id'
+    path: `${mockContentId}/test.txt`,
+    contentId: `${mockContentId}`,
+    isTemp: true,
+    createdBy: mockUserId
   };
-  const insertTemp = contentFilepaths.create(tempMockData);
+  const insertTemp = contentFilepaths.create(tempMockData).then(fileObj => {
+    saveMockFiles.push(fileObj._id);
+  });
 
 
 
-  return Promise.all([insertPersistent, insertTemp]);
+  return Promise.all([...insertPersistent, insertTemp]);
 };
 
 const removeMock = () => {
   contentFilepaths
-    .find({ query: { contentId: 'content_mock_id' } })
+    .find({ query: { contentId: `${mockContentId}` } })
     .then(res => {
       return Promise.all(
         res.data.map(mockData => contentFilepaths.remove(mockData._id))
@@ -52,96 +61,38 @@ describe('\'files/manage\' service', () => {
   before(insertMock);
   after(removeMock);
 
-  it('deletes files from DB', () => {
-    const service = app.service('files/manage');
-
-    const itemsToDelete = ['content_mock_id/index.html', 'content_mock_id/menue/clip_6_1.html'];
-    const itemsToSave = [];
-
-    return service.patch('content_mock_id', {
-        delete: itemsToDelete,
-        save: itemsToSave,
-        userId: 'user_mock_id'
-      })
-      .then((res) => {
-        assert.equal(res.status, 200);
-        return contentFilepaths.find({query: {
-          contentId: 'content_mock_id',
-          userId: 'user_mock_id',
-          fileIds: {$in: itemsToDelete}
-        }});
-      })
-      .then((deleted) => {
-        assert.equal(deleted.data.length, 0, 'files weren\'t deleted');
-      });
-  });
-
-  it('saves files into DB', () => {
-    const service = app.service('files/manage');
-
-    const itemsToDelete = [];
-    const itemsToSave = ['tmp/user_mock_id/content_mock_id/test.txt'];
-
-    return service.patch('content_mock_id', {
-        delete: itemsToDelete,
-        save: itemsToSave,
-        userId: 'user_mock_id'
-      })
-      .then((res) => {
-        assert.equal(res.status, 200);
-        const findSavedSrc = contentFilepaths.find({query: {
-          contentId: 'content_mock_id',
-          userId: 'user_mock_id',
-          isTemporary: true,
-          fileIds: {$in: itemsToSave}
-        }});
-
-        const findSaved = contentFilepaths.find({query: {
-          contentId: 'content_mock_id',
-          userId: 'user_mock_id',
-          isTemporary: false,
-          fileIds: {$in: itemsToSave.map(entry => entry.replace('tmp/user_mock_id/', ''))}
-        }});
-
-        return Promise.all([findSavedSrc, findSaved]);
-      })
-      .then(([savedSrc, saved]) => {
-        assert.equal(savedSrc.data.length, 0, 'src wasn\'t deleted');
-        assert.equal(saved.data.length, 1, 'new file wasn\'t moved to destination');
-      });
-  });
-
   it('manages files in DB', () => {
     const service = app.service('files/manage');
 
-    const itemsToDelete = ['content_mock_id/index.html', 'content_mock_id/menue/clip_6_1.html'];
-    const itemsToSave = ['tmp/user_mock_id/content_mock_id/test.txt'];
+    const itemsToDelete = [deleteMockFiles[0], deleteMockFiles[deleteMockFiles.length - 1]];
+    const itemsToSave = saveMockFiles;
 
-    return service.patch('content_mock_id', {
+    return service.patch(`${mockContentId}`, {
         delete: itemsToDelete,
         save: itemsToSave,
-        userId: 'user_mock_id'
+        userId: mockUserId
       })
       .then((res) => {
         assert.equal(res.status, 200);
         const findDeleted = contentFilepaths.find({query: {
-          contentId: 'content_mock_id',
-          userId: 'user_mock_id',
-          fileIds: {$in: itemsToDelete}
+          _id: {$in: itemsToDelete},
+          contentId: `${mockContentId}`,
+          createdBy: mockUserId,
+          isTemp: false,
         }});
 
         const findSavedSrc = contentFilepaths.find({query: {
-          contentId: 'content_mock_id',
-          userId: 'user_mock_id',
-          isTemporary: true,
-          fileIds: {$in: itemsToSave}
+          _id: {$in: itemsToSave},
+          contentId: `${mockContentId}`,
+          createdBy: mockUserId,
+          isTemp: true,
         }});
 
         const findSaved = contentFilepaths.find({query: {
-          contentId: 'content_mock_id',
-          userId: 'user_mock_id',
-          isTemporary: false,
-          fileIds: {$in: itemsToSave.map(entry => entry.replace('tmp/user_mock_id/', ''))}
+          _id: {$in: itemsToSave},
+          contentId: `${mockContentId}`,
+          createdBy: mockUserId,
+          isTemp: false,
         }});
 
         return Promise.all([findDeleted, findSavedSrc, findSaved]);
@@ -149,7 +100,7 @@ describe('\'files/manage\' service', () => {
       .then(([deleted, savedSrc, saved]) => {
         assert.equal(deleted.data.length, 0, 'files weren\'t deleted');
         assert.equal(savedSrc.data.length, 0, 'src wasn\'t deleted');
-        assert.equal(saved.data.length, 1, 'new file wasn\'t moved to destination');
+        assert.equal(saved.data.length, itemsToSave.length, 'new file wasn\'t moved to destination');
       });
   });
 });
