605f77e296d7376806657fba8d5ec19866887223
Adrian Jost <adrianjost@users.noreply.github.com>
2019-03-13 14:51:05 +0100

    remove broken require

--- src/services/index.js
+++ src/services/index.js
@@ -2,7 +2,6 @@ const files = require('./files/files.service.js');
 const resource = require('./resource/resource.service.js');
 const search = require('./search/search.service.js');
 const user = require('./user/user.service.js');
-const reserved_Ids = require('./reserved_Ids/reserved_Ids.service.js');
 const content_filepaths = require('./content_filepaths/content_filepaths.service.js');
 
 module.exports = function () {
@@ -11,6 +10,5 @@ module.exports = function () {
   app.configure(resource);
   app.configure(search);
   app.configure(user);
-  app.configure(reserved_Ids);
   app.configure(content_filepaths);
 };
