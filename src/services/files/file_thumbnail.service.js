aef12cf1f7758733cd6171d970e974894d1dc480
Adrian Jost <adrianjost@users.noreply.github.com>
2019-04-03 11:14:35 +0200

    add authentication to pichasso

--- src/services/files/file_thumbnail.service.js
+++ src/services/files/file_thumbnail.service.js
@@ -1,35 +1,66 @@
+const errors = require('@feathersjs/errors');
 const request = require('request');
-const { uploadFile } = require('./file_upload.service');
+const rpn = require('request-promise-native');
+
 const config = require('config');
 const pichassoConfig = config.get('pichasso');
+const { uploadFile } = require('./file_upload.service');
 
+const api = rpn.defaults({
+  resolveWithFullResponse: true
+});
+
+class PreconditionFailed extends errors.FeathersError {
+  constructor(message, data) {
+    super(message, 'precondition-failed', 412, 'PreconditionFailed', data);
+  }
+}
 
 class ThumbnailService {
   constructor(app) {
     this.app = app;
   }
 
-  async patch(resourceId, data, params) {
+  async patch(resourceId /*, data, params */) {
     if (!pichassoConfig.enabled) {
-      return 'DISABLED :\'('; // TODO
+      throw new errors.Unavailable('{status: 503, message: "service disabled"}');
     }
 
     const resource = await this.app.service('resources').get(resourceId);
 
     if(resource.thumbnail){
-      return 'SCHON DA ;)'; // TODO
+      throw new PreconditionFailed('{status: 412, message: "thumbnail already exists"}');
     }
 
+    const pichassoHost = `${pichassoConfig.url}:${pichassoConfig.port}`;
+
+    // get auth token from pichasso
+    const verifyUrl = `${pichassoHost}/thumbnail/verify/${pichassoConfig.token}/${encodeURIComponent(resource.url)}`;
+    const verifyResponse = await api.get(verifyUrl);
+
+    // verify token
+    if (verifyResponse.statusCode !== 200) {
+      throw new errors.GeneralError('{status: 500, message:"verification failed getting thumbnail url"}');
+    }
+    const authtoken = verifyResponse.body;
+    if(!authtoken || authtoken.length !== 6){
+      throw new errors.GeneralError('{status: 500, message: "received token seems to be wrong"}');
+    }
+
+    // create thumbnail
     const fileId = await uploadFile({
       app: this.app,
       userId: undefined,
       resourceId,
       uploadPath: resourceId+'/thumbnail.png',
-      sourceStream: request(`${pichassoConfig.url}:${pichassoConfig.port}/thumbnail?file=${resource.url}`)
+      sourceStream: request(`${pichassoHost}/thumbnail?auth=${authtoken}&file=${resource.url}`)
     });
+    // publish thumbnail
     await this.app.service('resource_filepaths').patch(fileId, { isTemp: false });
+    // patch thumbnail into resource
     await this.app.service('resources').patch(resourceId, { thumbnail: `${config.get('protocol')}://${config.get('host')}:${config.get('port')}/files/get/${resourceId}/thumbnail.png` });
-    return 'CREATED :D'; // TODO
+
+    return `{status: 200, message: ${fileId}}`;
   }
 }
 
