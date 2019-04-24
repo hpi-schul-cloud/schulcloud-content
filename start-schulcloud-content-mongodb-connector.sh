33a3b8dbc34274910bd05ad4d42e6d06c162e118
Adrian Jost <adrianjost@users.noreply.github.com>
2019-04-24 13:28:31 +0200

    fix some merge conflicts

--- start-schulcloud-content-mongodb-connector.sh
+++ start-schulcloud-content-mongodb-connector.sh
@@ -2,12 +2,14 @@
 
 if [ $1 == "seed" ]
 then
-    mongo --host=localhost --port=27017 --eval "rs.initiate({ _id : \"rs\", members: [ { _id : 0, host : \"localhost:27017\" } ] })"
-    mongoimport --host=localhost --port=27017 --db schulcloud_content --collection resources --file ./backup/schulcloud_content/resources.json
+    mongo --host=$MONGO_URI --port=$MONGO_PORT --eval "rs.initiate({ _id : \"rs\", members: [ { _id : 0, host : \"$MONGO_URI:$MONGO_PORT\" } ] })"
+    mongoimport --host=$MONGO_URI --port=$MONGO_PORT --db schulcloud_content --collection resources --file /usr/src/connector/backup/schulcloud_content/resources.json
 
-    mongo-connector -m localhost:27017 -t localhost:9200 -d elastic2_doc_manager -v
+    cd /
+    mongo-connector -m mongodb:27017 -t elasticsearch:9200 -d elastic2_doc_manager -v
 fi
 if [ $1 == "start" ]
 then
-    mongo-connector -m localhost:27017 -t localhost:9200 -d elastic2_doc_manager -v
+    cd /
+    mongo-connector -m mongodb:27017 -t elasticsearch:9200 -d elastic2_doc_manager -v
 fi
