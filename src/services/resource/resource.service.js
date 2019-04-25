c2bb5df3329d4db1030abbbd830783502ac23022
Adrian Jost <adrianjost@users.noreply.github.com>
2019-04-25 16:22:21 +0200

    add bulk service

--- src/services/resource/resource.service.js
+++ src/services/resource/resource.service.js
@@ -1,9 +1,10 @@
-// Initializes the `content` service on path `/content`
+// Initializes the `resources` service on path `/resources`
 const createService = require('feathers-mongoose');
 const createModel = require('../../models/resource.model');
 const hooks = require('./resource.hooks');
 
 const { ResourceSchemaService } = require('./resource_getResourceSchema.service.js');
+const { ResourceBulkService } = require('./resource_bulk.service.js');
 
 
 module.exports = function () {
@@ -20,12 +21,13 @@ module.exports = function () {
   // delivers resource schema for frontend validation
   app.use('/resources/resource-schema',  new ResourceSchemaService(app));
 
+  app.use('/resources/bulk',  new ResourceBulkService(app));
+  const bulkService = app.service('resources/bulk');
+  bulkService.hooks(hooks);
 
   // Initialize our service with any options it requires
   app.use('/resources', createService(options));
-
   // Get our initialized service so that we can register hooks and filters
   const service = app.service('resources');
-
   service.hooks(hooks);
 };
---
+++ src/services/resource/resource_bulk.service.js
@@ -0,0 +1,25 @@
+class ResourceBulkService {
+  constructor(app) {
+    this.app = app;
+  }
+
+  async create(data, params) {
+    return this.app.service('resources').create(data, params);
+  }
+
+  async patch(id, data, { query }) {
+    if (id) {
+      throw new Error('use /resources for singular updates');
+    }
+    const ids = await this.app.service('search').find({ query });
+    // TODO MAYBE CHUNK REQUEST WITH TRANSACTIONS
+    const result = await this.app
+      .service('resources')
+      .patch(null, data, { query: { _id: { $in: ids } } });
+    return result;
+  }
+}
+
+module.exports = {
+  ResourceBulkService
+};
