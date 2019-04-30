c4cc5a37027fe9f3a70c3d3f9c152bcba47afa89
Adrian Jost <adrianjost@users.noreply.github.com>
2019-04-30 14:28:34 +0200

    add bulk delete service

--- src/services/resource/resource_bulk.service.js
+++ src/services/resource/resource_bulk.service.js
@@ -18,6 +18,18 @@ class ResourceBulkService {
       .patch(null, data, { query: { _id: { $in: ids } } });
     return result;
   }
+
+  async remove(id, {query }) {
+    if (id) {
+      throw new Error('use /resources for singular updates');
+    }
+    const ids = await this.app.service('search').find({ query });
+    // TODO MAYBE CHUNK REQUEST WITH TRANSACTIONS
+    const result = await this.app
+      .service('resources')
+      .remove(null, { query: { _id: { $in: ids } } });
+    return result;
+  }
 }
 
 module.exports = {
