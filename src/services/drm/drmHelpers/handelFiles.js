f93d22b83fa1824d041594a49633068a2cc3647c
Xhenet <elias.maass@student.hpi.de>
2019-06-05 15:19:53 +0200

    More Video DRM Refactoring

--- src/services/drm/drmHelpers/handelFiles.js
+++ src/services/drm/drmHelpers/handelFiles.js
@@ -6,46 +6,40 @@ const Magic = require('mmmagic').Magic;
 const download = require('download-file');
 const config = require('config');
 const drmConfig = config.get('DRM');
-const rmdir = require('rmdir');
 
 const getResourceFileList = (app, resourceId) => {
-    const getFilePathPromise = app
-      .service('resource_filepaths')
-      .find({
-        query: {
-          resourceId: resourceId
-        }
-      })
-      .then(async fileObjects => {
-        const currentFiles = fileObjects.data;
-        return currentFiles.map(currentFile => {
-          return {
-            id: currentFile._id,
-            path: currentFile.path,
-            drmProtection: currentFile.drmProtection
-          };
-        });
+  const getFilePathPromise = app
+    .service('resource_filepaths')
+    .find({
+      query: {
+        resourceId: resourceId
+      }
+    })
+    .then(async fileObjects => {
+      const currentFiles = fileObjects.data;
+      return currentFiles.map(currentFile => {
+        return {
+          id: currentFile._id,
+          path: currentFile.path,
+          drmProtection: currentFile.drmProtection
+        };
       });
-    return getFilePathPromise;
-  };
+    });
+  return getFilePathPromise;
+};
+
 const videoCleanupOnDelete = (app, resourceId) =>{
   app
   .service('videoId')
   .find({ query: { resourceId: resourceId } })
   .then(searchResults => {
     const currentFiles = searchResults.data;
-    const pathWorking =
-          drmConfig.absoluteLocalStoragePath +
-          '\\' +
-          drmConfig.workingDir +
-          '\\' +
-          currentFiles[0].videoId;
-    rmdir(pathWorking);
     currentFiles.map(currentFile =>
       app.service('videoId').remove(currentFile._id)
     );
   });
 };
+
 const uploadAndDelete = async (app, resourceFileList, sourceFolderPath) => {
   await Promise.all(
     resourceFileList.map(async element => {
@@ -55,7 +49,7 @@ const uploadAndDelete = async (app, resourceFileList, sourceFolderPath) => {
           await app.service('resource_filepaths').create(resource).then(async(newRecource)=>{
             let sourceStream = fs.createReadStream(element.outputFilePath);
             await promisePipe(sourceStream, getUploadStream(newRecource._id));
-            app.service('resource_filepaths').patch(element.id.toString(),{path: '/'+drmConfig.originalFilesFolderName+element.path }).then(async()=>{
+            app.service('resource_filepaths').patch(element.id.toString(),{path: '/' + drmConfig.originalFilesFolderName+element.path }).then(async()=>{
             });
           });
         });
@@ -66,6 +60,7 @@ const uploadAndDelete = async (app, resourceFileList, sourceFolderPath) => {
       }
     })
   );
+
   const isEmpty = emptyDir.sync(sourceFolderPath);
   if (isEmpty) {
     fs.rmdir(sourceFolderPath, err => {
