37fc09991ac0d6732b11d784acfe9190ac3c0270
Kathi <katharina.blass@student.hpi.de>
2019-06-05 16:56:52 +0200

    fix internal bug

--- src/services/users/users.hooks.js
+++ src/services/users/users.hooks.js
@@ -6,6 +6,10 @@ const {
 } = require('@feathersjs/authentication-local').hooks;
 
 const checkRole = ({permittedRoles}) => async hook => {
+  // skip for internal calls
+  if (typeof hook.params.provider === 'undefined') {
+    return hook;
+  }
   const userModel = hook.app.get('mongooseClient').model('users');
   return new Promise((resolve, reject) => {
     userModel.findById(hook.params.user._id, function (err, user) {
@@ -22,6 +26,10 @@ const checkRole = ({permittedRoles}) => async hook => {
 };
 
 const restrictAccessByProvider = hook => {
+  // skip for internal calls
+  if (typeof hook.params.provider === 'undefined') {
+    return hook;
+  }
   if(hook.params.user.role !== 'superhero') {
     hook.params.query = { providerId: hook.params.user.providerId };
   }
