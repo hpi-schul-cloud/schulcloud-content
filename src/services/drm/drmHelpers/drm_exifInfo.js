9840dae5a5a6358ccd8993a712f8323ecb199835
Xhenet <elias.maass@student.hpi.de>
2019-06-05 10:55:19 +0200

    Exifdata Bugfix
    Exifdata only is modified if there is somthing to be modified

--- src/services/drm/drmHelpers/drm_exifInfo.js
+++ src/services/drm/drmHelpers/drm_exifInfo.js
@@ -1,5 +1,9 @@
 const writeExifData = async (ep, data, path) => {
+  if(Object.keys(data).length === 0){
+    return;
+  }
   await ep.writeMetadata(path, data, ['-File:all', 'overwrite_original']);
+  return;
 };
 
 module.exports = {
