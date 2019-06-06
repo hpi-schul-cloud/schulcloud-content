3bdc4aaa1b7e19a4e75edc8b1120210218b727e8
Xhenet <elias.maass@student.hpi.de>
2019-06-06 16:01:41 +0200

    Bugfix redirection

--- src/services/drm/drm_videoRedirect.service.js
+++ src/services/drm/drm_videoRedirect.service.js
@@ -1,11 +1,22 @@
+const config = require('config');
+const drmConfig = config.get('DRM');
+
 class VideoRedirectService {
   constructor(app) {
     this.app = app;
   }
   async get(resourceId /*obj*/) {
-    return await this.app.service('videoId').find({query:{resourceId: resourceId}}).then((response)=>{
+    return await this.app.service('videoId').find({query:{resourceId: resourceId}}).then(async (response)=>{
       if (response.total != 0) {
-        return response.data[0].videoId;
+        return await this.app.service('resources').get(response.data[0].resourceId).then((resource)=>{
+          let file = resource.url.split('/');
+          let extension = file.slice(file.length-1,file.length)[0].split('.');
+          extension = extension.slice(extension.length-1,extension.length)[0];
+          if (drmConfig.videoFileTypes.includes(extension)) {
+            return response.data[0].videoId;
+          }
+          return false;
+        });
       }
       return false;
     });
