d6fe2c533a1cc4bfa18cc7b3760dc621fc920658
Kathi <katharina.blass@student.hpi.de>
2019-06-06 16:30:03 +0200

    update seed.sh & permissions for /search [WIP]

--- src/services/search/search.hooks.js
+++ src/services/search/search.hooks.js
@@ -1,4 +1,7 @@
 const { populateResourceUrls } = require('../../hooks/populateResourceUrls');
+const errors = require('@feathersjs/errors');
+const authenticateHook = require('../../authentication/authenticationHook');
+
 
 const allowDisableLimit = hook => {
   if (hook.params.query.$limit === '-1') {
@@ -8,10 +11,52 @@ const allowDisableLimit = hook => {
   return hook;
 };
 
+const restrictToProviderOrToPublicResources = hook => {
+  async function getUser(userId) {
+    const userModel = hook.app.get('mongooseClient').model('users');
+    return new Promise((resolve, reject) => {
+      userModel.findById(userId, function (err, user) {
+        if(err) { reject(new errors.GeneralError(err)); }
+        if(!user) {
+          reject(new errors.NotFound('User not found'));
+        }
+        return resolve(user);
+      });
+    });
+  }
+
+  return getUser(hook.params.user._id)
+  .then((user)=>{
+    if(user.role !== 'superhero') {
+      hook.params.query = {$or: [
+        { providerId: user.providerId.toString() },
+        { isPublished: true }
+      ]};
+    }
+    return hook;
+  });
+};
+
+const restrictToPublicResources = hook => {
+  hook.params.query = { isPublished: true };
+  return hook;
+};
+
+// Lern-Store can only access public resources, authenticated users can access public resources and resources of their company
+const checkPermissionsAfterAuthentication = hook => {
+  try {
+    return authenticateHook()(hook).then(result => {
+      return restrictToProviderOrToPublicResources(result);
+    });
+  } catch (error) {
+    return restrictToPublicResources(hook);
+  }
+};
+
 module.exports = {
   before: {
     all: [],
-    find: [allowDisableLimit],
+    find: [checkPermissionsAfterAuthentication, allowDisableLimit],
     get: [],
     create: [],
     update: [],
