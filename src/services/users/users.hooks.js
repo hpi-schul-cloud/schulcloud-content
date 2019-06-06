318cf55d370b56c9c024dd0efbd293aba9ec7dc7
Kathi <katharina.blass@student.hpi.de>
2019-06-06 11:50:08 +0200

    permissions for service /users works (without role user)

--- src/services/users/users.hooks.js
+++ src/services/users/users.hooks.js
@@ -1,56 +1,70 @@
 const authenticateHook = require('../../authentication/authenticationHook');
+const errors = require('@feathersjs/errors');
 
 
 const {
   hashPassword, protect
 } = require('@feathersjs/authentication-local').hooks;
 
-const checkRole = ({permittedRoles}) => async hook => {
-  // skip for internal calls
-  if (typeof hook.params.provider === 'undefined') {
-    return hook;
-  }
+const checkUserHasRole = (permittedRoles) => async hook => {
   const userModel = hook.app.get('mongooseClient').model('users');
   return new Promise((resolve, reject) => {
     userModel.findById(hook.params.user._id, function (err, user) {
-      if (err || !user) { return reject(err); }
-      if (permittedRoles.includes(user.role)){
-          hook.params.user.role = user.role;
-          hook.params.user.providerId = user.providerId;
-          return resolve();
-      } else {
-        return reject('not permitted');
+      if(err) { reject(new errors.GeneralError(err)); }
+      if(!user) {
+        reject(new errors.NotFound('User not found'));
       }
+      if (!permittedRoles.includes(user.role)){
+        reject(new errors.Forbidden('Permissions missing'));
+      }
+      hook.params.user.role = user.role;
+      hook.params.user.providerId = user.providerId;
+      return resolve(hook);
     });
   });
 };
 
-const restrictAccessByProvider = hook => {
-  // skip for internal calls
-  if (typeof hook.params.provider === 'undefined') {
-    return hook;
-  }
+const restrictAccessToCurrentProvider = hook => {
   if(hook.params.user.role !== 'superhero') {
     hook.params.query = { providerId: hook.params.user.providerId };
   }
   return hook;
 };
 
-const restrictCreationByProvider = hook => {
+const restrictCreationToCurrentProvider = hook => {
   if(hook.params.user.role !== 'superhero') {
     hook.data.providerId = hook.params.user.providerId;
+    hook.data.role = 'user';
   }
   return hook;
 };
 
+const ckeckUserHasPermission = hook => {
+  return checkUserHasRole(['admin', 'superhero'])(hook)
+  .then(()=>{
+    if(hook.method == 'create') {
+      restrictCreationToCurrentProvider(hook);
+    } else {
+      restrictAccessToCurrentProvider(hook);
+    }
+  });
+};
+
+const skipInternal = (method) => (hook) => {
+  if (typeof hook.params.provider === 'undefined') {
+    return hook;
+  }
+  return method(hook);
+};
+
 module.exports = {
   before: {
-    all: [ authenticateHook(), checkRole({ permittedRoles: ['admin', 'superhero'] }) ],
-    find: [ restrictAccessByProvider ],
-    get: [ restrictAccessByProvider ],
-    create: [ restrictCreationByProvider, hashPassword() ],
-    patch: [ restrictAccessByProvider, hashPassword() ],
-    remove: [ restrictAccessByProvider ]
+    all: [ authenticateHook(), skipInternal(ckeckUserHasPermission) ],
+    find: [  ],
+    get: [  ],
+    create: [ hashPassword() ],
+    patch: [  hashPassword() ],
+    remove: [  ]
   },
 
   after: {
