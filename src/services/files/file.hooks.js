95185995bd401cd4499756d3dd70bb3cc83dc089
Kathi <katharina.blass@student.hpi.de>
2019-06-11 17:48:27 +0200

    permissions ready :boom:  (needs some refactoring!)
    - add permissions for /files/get*

--- src/services/files/file.hooks.js
+++ src/services/files/file.hooks.js
@@ -1,6 +1,7 @@
 const commonHooks = require('feathers-hooks-common');
 const defaultHooks = require('./file_default.hook.js');
 const authenticateHook = require('../../authentication/authenticationHook');
+const { unifySlashes } = require('../../hooks/unifySlashes');
 
 
 const errors = require('@feathersjs/errors');
@@ -99,10 +100,61 @@ const restrictResourceToCurrentProvider = async hook => {
   return hook;
 };
 
+const restrictToProviderOrToPublicResources = hook => {
+  if(hook.params.user.role !== 'superhero') {
+    if(!(hook.params.resource.providerId.toString() === hook.params.user.providerId.toString() || hook.params.resource.isPublished === true)) {
+      throw new errors.Forbidden('Permissions missing');
+    }
+  }
+  return hook;
+};
+
+const restrictToPublicResources = hook => {
+  if(hook.params.resource.isPublished != true) {
+    throw new errors.Forbidden('Permissions missing');
+  }
+  return hook;
+};
+
+const getResource = async hook => {
+  const path = hook.params.route[0];
+  let [resourceId] = unifySlashes(path)
+    .replace(/^\/+/g, '') // remove leading slashes
+    .split('/');
+  const resource = await hook.app.service('resources').get(resourceId);
+  hook.params.resource = resource;
+  return hook;
+};
+
+// Lern-Store can only access public resources, authenticated users can access public resources and resources of their company
+const checkPermissionsAfterAuthentication = hook => {
+  return getResource(hook)
+  .then((resultHook)=>{
+    return authenticateHook()(resultHook)
+    .then(result => {
+      return getCurrentUserData(result).then((res)=>{
+          return restrictToProviderOrToPublicResources(res);
+      });
+    })
+    .catch(()=>{
+      return restrictToPublicResources(hook);
+    });
+  });
+};
+
+const skipInternal = (method) => (hook) => {
+if (typeof hook.params.provider === 'undefined') {
+  return hook;
+}
+return method(hook);
+};
+
 const distributionHooks = {
   ...defaultHooks,
   before: {
-    find: []
+    find: [
+      skipInternal(checkPermissionsAfterAuthentication)
+    ]
   }
 };
 
