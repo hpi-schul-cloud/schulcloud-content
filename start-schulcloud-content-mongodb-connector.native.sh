3434d89036b3838bc8e9ab2d6e61a323265e4981
Kathi <katharina.blass@student.hpi.de>
2019-06-12 13:08:36 +0200

    Update start-schulcloud-content-mongodb-connector.native.sh

--- start-schulcloud-content-mongodb-connector.native.sh
+++ start-schulcloud-content-mongodb-connector.native.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
+ELASTIC_HOST=${ELASTIC_HOST:="localhost"}
+ELASTIC_PORT=${ELASTIC_PORT:="9200"}
 
-mongo-connector --auto-commit-interval=0 -m localhost:27017 -t localhost:9200 -d elastic2_doc_manager -v
+mongo-connector --auto-commit-interval=0 -m localhost:27017 -t $ELASTIC_HOST:$ELASTIC_PORT -d elastic2_doc_manager -v
